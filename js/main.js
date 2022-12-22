369fb22675572261f5022c8b10c847c2f50308b1
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-22 19:56:38 +0500

    About desktop

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 400,
+  slidesPerView: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
