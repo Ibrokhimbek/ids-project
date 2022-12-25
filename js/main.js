c28bc19ac6e80cef263286e124332037b823fbc6
Ibrokhimbek <ibrokhim1348@gmail.com>
2022-12-26 01:20:02 +0500

    header was added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const menuBtns = document.querySelectorAll("#menu__btn");
+const header = document.querySelector(".header");
+
+menuBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    header.classList.toggle("show");
+  });
+});
+
 const swiper = new Swiper(".swiper", {
   spaceBetween: 400,
   slidesPerView: 1,
